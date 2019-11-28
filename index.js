f476eb090110afc88bcae261ed99f2f078e6cd0b
Julie Nguyen <julie-nguyen@live.com>
2019-11-28 19:56:27 +0000

    first implementation of sumItems

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  
+  // Base case if the array is empty
+  if (array.length === 0) {
+    return 0;
+  
+  // if the array has an array as the first element
+  } else if (Array.isArray(array[0])) {
+    let nestedArray = array[0];
+    array.shift();
+    return sumItems(nestedArray) + sumItems(array);
+  
+  // If the first element is not an array
+  } else {
+    let value = array[0];
+    array.shift();
+    return value + sumItems(array);
+  }
 }
+
 
 module.exports = sumItems;
